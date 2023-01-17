a0ef24d7d4caaa1f57903d76617ba04542445a10
PreetiNegi2709 <105388761+PreetiNegi2709@users.noreply.github.com>
2023-01-17 20:59:10 +0530

    third push

--- horo_js.js
+++ horo_js.js
@@ -11,116 +11,133 @@ const get_bd_date_Value = ()=>{
 
 let sign
 const horo_sign = (birth_date,birth_month)=>{
-    if (birth_month=="dec" || birth_month=="jan"){
-        if (birth_month=="dec" && birth_date>=22){
-            sign="Capricorn"
-        }
-        else if (birth_month=="jan" && birth_date<=19){
-            sign="Capricorn"
-        }
-        else{}
-    }
-    else if (birth_month=="jan" || birth_month=="feb"){
-        if (birth_month=="jan" && birth_date>=20){
-            sign="Aquarius"
-        }
-        else if (birth_month=="feb" && birth_date<=17){
-            sign="Aquarius"
-        }
-        else{}
-    }
-    else if (birth_month=="feb" || birth_month=="march"){
-        if (birth_month=="feb" && birth_date>=18){
-            sign="Pisces"
-        }
-        else if (birth_month=="march" && birth_date<=19){
-            sign="Pisces"
-        }
-        else{}
-    }
-    else if (birth_month=="march" || birth_month=="april"){
-        if (birth_month=="march" && birth_date>=20){
+    if ((birth_month=="dec" && birth_date>=23) || (birth_month=="jan" && birth_date<=20))
+        sign="Capricorn"
+        
+    else if ((birth_month=="jan" && birth_date>=21) || (birth_month=="feb" && birth_date<=17))
+        sign="Aquarius"
+        
+    else if ((birth_month=="feb" && birth_date>=18) || (birth_month=="march" && birth_date<=19))
+        sign="Pisces"
+    
+    else if ((birth_month=="march" && birth_date>=20) || (birth_month=="april" && birth_date<=19))
             sign="Aries"
-        }
-        else if (birth_month=="april" && birth_date<=19){
-            sign="Aries"
-        }
-        else{}
-    }
-    else if (birth_month=="april" || birth_month=="may"){
-        if (birth_month=="april" && birth_date>=20){
+        
+    else if ((birth_month=="april" && birth_date>=20) || (birth_month=="may" && birth_date<=20))
             sign="Taurus"
-        }
-        else if (birth_month=="may" && birth_date<=20){
-            sign="Taurus"
-        }
-        else{}
-    }
-    else if (birth_month=="may" || birth_month=="june"){
-        if (birth_month=="may" && birth_date>=21){
-            sign="Gemini"
-        }
-        else if (birth_month=="june" && birth_date<=20){
-            sign="Gemini"
-        }
-        else{}
-    }
-    else if (birth_month=="june" || birth_month=="july"){
-        if (birth_month=="june" && birth_date>=21){
-            sign="Cancer"
-        }
-        else if (birth_month=="july" && birth_date<=22){
-            sign="Cancer"
-        }
-        else{}
-    }
-    else if (birth_month=="july" || birth_month=="august"){
-        if (birth_month=="july" && birth_date>=23){
-            sign="Leo"
-        }
-        else if (birth_month=="august" && birth_date<=22){
-            sign="Leo"
-        }
-        else{}
-    }
-    else if (birth_month=="august" || birth_month=="sep"){
-        if (birth_month=="august" && birth_date>=23){
-            sign="Virgo"
-        }
-        else if (birth_month=="sep" && birth_date<=22){
-            sign="Virgo"
-        }
-        else{}
-    }
-    else if (birth_month=="sep" || birth_month=="oct"){
-        if (birth_month=="sep" && birth_date>=23){
-            sign="Libra"
-        }
-        else if (birth_month=="oct" && birth_date<=22){
-            sign="Libra"
-        }
-        else{}
-    }
-    else if (birth_month=="oct" || birth_month=="nov"){
-        if (birth_month=="oct" && birth_date>=23){
-            sign="Scorpio"
-        }
-        else if (birth_month=="nov" && birth_date<=21){
-            sign="Scorpio"
-        }
-        else{}
-    }
-    else if (birth_month=="nov" || birth_month=="dec"){
-        if (birth_month=="nov" && birth_date>=22){
-            sign="Sagittarius"
-        }
-        else if (birth_month=="dec" && birth_date<=21){
-            sign="Sagittarius"
-        }
-        else{}
-    }
-    else{
-        sign="you entered wrong date"
-    }
-} 
 
+    else if ((birth_month=="may" && birth_date>=21) || (birth_month=="june" && birth_date<=20))
+            sign="Gemini"
+
+    else if ((birth_month=="june" && birth_date>=21) || (birth_month=="july" && birth_date<=22))
+            sign="Cancer"
+    
+    else if ((birth_month=="july" && birth_date>=23) || (birth_month=="august" && birth_date<=22))
+            sign="Leo"
+  
+    else if ((birth_month=="august" && birth_date>=23) || (birth_month=="sep" && birth_date<=22))
+            sign="Virgo"
+            
+    else if ((birth_month=="sep" && birth_date>=23) || (birth_month=="oct" && birth_date<=22))
+            sign="Libra"
+
+    else if ((birth_month=="oct" && birth_date>=23) || (birth_month=="nov" && birth_date<=21))
+            sign="Scorpio"
+ 
+    else if ((birth_month=="nov" && birth_date>=22) || (birth_month=="dec" && birth_date<=22))
+            sign="Sagittarius"
+
+    else
+        sign="you entered wrong date"
+    
+}
+
+//colour change opf sign_box when curson enter\
+// let s_box= document.getElementsByClassName("sign_box")
+// row1
+// sign_box_cap.onmouseover=function(){
+//     document.getElementById("sign_box_cap").style.backgroundColor="#AA336A"
+// }
+// sign_box_cap.onmouseout=function(){
+//     document.getElementById("sign_box_cap").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+// sign_box_aqu.onmouseover=function(){
+//     document.getElementById("sign_box_aqu").style.backgroundColor="#AA336A"
+// }
+// sign_box_aqu.onmouseout=function(){
+//     document.getElementById("sign_box_aqu").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+// sign_box_pis.onmouseover=function(){
+//     document.getElementById("sign_box_pis").style.backgroundColor="#AA336A"
+// }
+// sign_box_pis.onmouseout=function(){
+//     document.getElementById("sign_box_pis").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+// sign_box_ari.onmouseover=function(){
+//     document.getElementById("sign_box_ari").style.backgroundColor="#AA336A"
+// }
+// sign_box_ari.onmouseout=function(){
+//     document.getElementById("sign_box_ari").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+// // row2
+// sign_box_tau.onmouseover=function(){
+//     document.getElementById("sign_box_tau").style.backgroundColor="#AA336A"
+// }
+// sign_box_tau.onmouseout=function(){
+//     document.getElementById("sign_box_tau").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+// sign_box_gem.onmouseover=function(){
+//     document.getElementById("sign_box_gem").style.backgroundColor="#AA336A"
+// }
+// sign_box_gem.onmouseout=function(){
+//     document.getElementById("sign_box_gem").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+// sign_box_can.onmouseover=function(){
+//     document.getElementById("sign_box_can").style.backgroundColor="#AA336A"
+// }
+// sign_box_can.onmouseout=function(){
+//     document.getElementById("sign_box_can").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+// sign_box_le.onmouseover=function(){
+//     document.getElementById("sign_box_le").style.backgroundColor="#AA336A"
+// }
+// sign_box_le.onmouseout=function(){
+//     document.getElementById("sign_box_le").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+// // row3
+// sign_box_vir.onmouseover=function(){
+//     document.getElementById("sign_box_vir").style.backgroundColor="#AA336A"
+// }
+// sign_box_vir.onmouseout=function(){
+//     document.getElementById("sign_box_vir").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+
+// sign_box_lib.onmouseover=function(){
+//         document.getElementById("sign_box_lib").style.backgroundColor="#AA336A"
+// }
+// sign_box_lib.onmouseout=function(){
+//         document.getElementById("sign_box_lib").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+// sign_box_sco.onmouseover=function(){
+//         document.getElementById("sign_box_sco").style.backgroundColor="#AA336A"
+// }
+// sign_box_sco.onmouseout=function(){
+//         document.getElementById("sign_box_sco").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
+
+// sign_box_sag.onmouseover=function(){
+//         document.getElementById("sign_box_sag").style.backgroundColor="#AA336A"
+// }
+// sign_box_sag.onmouseout=function(){
+//      document.getElementById("sign_box_sag").style.backgroundColor="rgb(94 75 119 / 77%)"
+// }
